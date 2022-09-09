395fcaf289a177b3f0c8030a2ff76d598eebba4a
Jack McMahon <60189032+jmcmah10@users.noreply.github.com>
2022-09-09 16:04:51 -0400

    Added Update.v with update function

---
+++ Update.v
@@ -0,0 +1,64 @@
+Require Import RuntimeDefinitions.
+From Coq Require Import Bool.Bool.
+
+Fixpoint eq_nat (n : nat) (m : nat) : bool :=
+  match n, m with
+  | 0, 0 => true
+  | 0, S _ => false
+  | S _, 0 => false
+  | S n1, S m1 => eq_nat n1 m1
+  end.
+
+Definition equal_enclave_IDs (e0 : enclave_ID) (e1 : enclave_ID) : bool :=
+  match e0, e1 with
+  | enclave_ID_active e0_val, enclave_ID_active e1_val => eq_nat e0_val e1_val
+  | _, _ => false
+  end.
+
+Fixpoint contains_way_id (w : way_ID) (T : PLRU_tree) : bool :=
+  match T with
+  | subtree sigma e T1 T2 => (contains_way_id w T1) || (contains_way_id w T2)
+  | subtree_leaf L => 
+    match L with
+    | leaf w' e => eq_nat w w'
+    end
+  end.
+
+Definition common_enclave (T1 : PLRU_tree) (T2 : PLRU_tree) : enclave_ID :=
+  match T1, T2 with
+  | subtree _ e _ _, subtree _ e' _ _ =>
+    match equal_enclave_IDs e e' with
+    | true => e
+    | false => enclave_ID_inactive
+    end
+  | _, _ => enclave_ID_inactive
+  end.
+
+Fixpoint update (T : PLRU_tree) (w : way_ID) (e: enclave_ID) : PLRU_tree :=
+  match T with
+  | subtree select_bit e0 T1 T2 =>
+    match equal_enclave_IDs e e0 with
+    | true =>
+      match (contains_way_id w T1) with
+      | true => subtree LMRU e (update T1 w e) T2
+      | false =>
+        match (contains_way_id w T2) with
+        | true => subtree RMRU e T1 (update T2 w e)
+        | false => subtree select_bit e T1 T2
+        end
+      end
+    | false => subtree select_bit (common_enclave T1 T2) (update T1 w e) (update T2 w e)
+    end
+  | subtree_leaf L =>
+    match L with
+    | leaf w' e' =>
+      match e' with
+      | enclave_ID_inactive =>
+        match eq_nat w w' with
+        | true => subtree_leaf (leaf w e)
+        | false => subtree_leaf L
+        end
+      | enclave_ID_active _ => subtree_leaf L
+      end
+    end
+  end.
